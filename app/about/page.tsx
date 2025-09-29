c82f2da8eb9b6fcb9b76b2b31cdabe41895d05f3
paprepadmin <elysia@papreplab.com>
2025-09-28 20:45:21 -0700

    Update page.tsx - update about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,10 +13,11 @@ const aboutColors = {
 const aboutText = {
   heading: "About Me",
   paragraphs: [
-    "Hello! My name is Elysia and I am a Physician Assistant practicing since 2019 in Orthopedics. I’m passionate about mentoring pre-PA students and those exploring healthcare careers—whether it’s a first step or a career pivot.",
-    "Applying to PA school was intimidating—I felt like an average applicant. But through strategic school choices, helpful mentors, and a strong personal statement, I earned an offer from my top-choice program.",
+    "Hello! My name is Elysia and I am a Physician Assistant/Associate practicing since 2019. I’m passionate about mentoring pre-PA students and those exploring healthcare careers; whether it’s a first step or a career pivot.",
+    "Applying to PA school was intimidating; I felt like an average applicant. But through strategic school choices, helpful mentors, and a strong personal statement, I earned an offer from my top-choice program.",
     "Since becoming certified, I’ve worked with PA students, interviewed applicants for my alma mater, and completed my Doctorate of Medical Science. I love guiding others through their journey to becoming a PA.",
     "Outside of work, I enjoy music, food, gardening, and time with family. I look forward to supporting you on your path to becoming a PA!",
+    "I look forward to guiding and supporting you on your path to becoming a successful PA student and future PA!",
   ],
   signature: "– Elysia Alva, DMSc, MPAS, PA-C",
   questionsHeading: "Still Have Questions?",
